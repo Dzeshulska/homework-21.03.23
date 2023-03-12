4c8f2bb567458f9fa13cc9a2d4a46c8327c527ea
Alla_Dseshulska <dzeshulska@gmail.com>
2023-03-12 11:17:14 +0200

    added function

--- task1.py
+++ task1.py
@@ -1,1 +1,95 @@
-auto_park
+import random
+auto_park = [
+    {   "lable" : "Seat",
+        "price" : "18000",
+        "wheels" : "4",
+        "color" : "Black",
+        "number" : random.randint(0,1000000000000),
+    },
+    {   "lable" : "Seat",
+        "price" : "18000",
+        "wheels" : "4",
+        "color" : "Black",
+        "number" : random.randint(0,1000000000000),
+    },
+    {   "lable" : "Seat",
+        "price" : "18000",
+        "wheels" : "4",
+        "color" : "Black",
+        "number" : random.randint(0,1000000000000),
+    },
+    {   "lable" : "Seat",
+        "price" : "18000",
+        "wheels" : "4",
+        "color" : "Black",
+        "number" : random.randint(0,1000000000000),
+    },
+    {   "lable" : "Seat",
+        "price" : "18000",
+        "wheels" : "4",
+        "color" : "Black",
+        "number" : random.randint(0,1000000000000),
+    },
+    {   "lable" : "Seat",
+        "price" : "18000",
+        "wheels" : "4",
+        "color" : "Black",
+        "number" : random.randint(0,1000000000000),
+    },
+    {   "lable" : "Seat",
+        "price" : "18000",
+        "wheels" : "4",
+        "color" : "Black",
+        "number" : random.randint(0,1000000000000),
+    },
+    {   "lable" : "Seat",
+        "price" : "18000",
+        "wheels" : "4",
+        "color" : "Black",
+        "number" : random.randint(0,1000000000000),
+    },
+    {   "lable" : "Seat",
+        "price" : "18000",
+        "wheels" : "4",
+        "color" : "Black",
+        "number" : random.randint(0,1000000000000),
+    },
+    {   "lable" : "Seat",
+        "price" : "18000",
+        "wheels" : "4",
+        "color" : "Black",
+        "number" : random.randint(0,1000000000000),
+    },
+    {   "lable" : "Seat",
+        "price" : "18000",
+        "wheels" : "4",
+        "color" : "Black",
+        "number" : random.randint(0,1000000000000),
+    },
+]
+
+i = 0
+for i in range(len(auto_park)) :
+    print(auto_park[i])
+
+def auto_park(lable, price, wheels, color, number):
+    return([
+    {    "lable": "Enter lable of car",
+         "Price" : "Enter price of car",
+         "wheels" : "Enter wheels of car",
+         "color" : "Enter color of car,"
+    }
+    ])
+
+i = 0
+for i in range(len(auto_park)) :
+    if len(auto_park) > 10 :
+        print(auto_park[i])
+
+
+
+
+
+
+
+
